dd5ff723bb937bec229d4477cf08db0148d984f2
rafalpienkowski <rafal.pienkowski@wp.pl>
2017-10-21 15:28:35 +0200

    changes

--- js/my.js
+++ js/my.js
@@ -1,7 +1,12 @@
 
 $(document).ready(function(){
 
-    for(var i = 1; i <= 9; i++){
-        $("#gallery" + i ).append("<img src='img/" + i + "-min.jpg' class='img-responsive' alt='img'>");
+    for(var i = 1; i <= 11; i++){
+        if (i === 7 || i === 8 || i === 10){
+            $("#gallery" + i ).append("<img src='img/" + i + "-min.jpg' class='img-responsive' alt='obraz' style='padding-left:25%'>");
+        }
+        else{
+            $("#gallery" + i ).append("<img src='img/" + i + "-min.jpg' class='img-responsive' alt='obraz'>");
+        }
     }
 });
